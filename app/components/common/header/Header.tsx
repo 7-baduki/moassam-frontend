8999d42f5e3de6736a907a2a5e64e7f3958392e9
youngsoon12 <enejwk789@gmail.com>
2026-04-22 17:05:26 +0900

    style: 텍스트 크기 및 자간 수정 (#4)

--- app/components/common/header/Header.tsx
+++ app/components/common/header/Header.tsx
@@ -8,25 +8,25 @@ interface HeaderProps {
 export default function Header({ isLoggedIn = false }: HeaderProps) {
   return (
     <header className="flex h-16 items-center justify-between px-20">
-      <div className="flex items-center gap-[34px]">
+      <div className="flex items-center gap-8.5">
         <Link href="/">
           <HeaderLogo />
         </Link>
-        <nav className="flex items-center gap-[34px]">
-          <Link href="/ai" className="text-pink-500">
+        <nav className="flex items-center gap-8.5 text-base leading-[140%] font-medium tracking-[-0.02em]">
+          <Link href="/ai" className="font-semibold text-pink-500">
             AI 일지 생성
           </Link>
           <Link href="/community">커뮤니티</Link>
           <Link href="/my-class">내 수업</Link>
         </nav>
       </div>
-      <div className="flex items-center gap-[24px]">
+      <div className="flex items-center gap-6">
         {isLoggedIn ? (
           <>{/* 로그인 아이콘 추가 예정 */}</>
         ) : (
-          <button className="rounded border border-pink-500 px-[14px] py-[6px] text-sm text-pink-500">
+          <button className="rounded border border-pink-500 px-3.5 py-1.5 text-sm text-pink-500">
             간편로그인
-          </button>
+          </button> // 공통 컴포넌트가 생성되면 수정 할 예정
         )}
       </div>
     </header>

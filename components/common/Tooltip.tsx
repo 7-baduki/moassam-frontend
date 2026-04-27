a02ca00eb96c8ca288fccec7c4b227e8b97aaa18
youngsoon12 <enejwk789@gmail.com>
2026-04-27 18:17:38 +0900

    fix: Tooltip 컴포넌트의 w-32.5 고정 제거 (#15)

--- components/common/Tooltip.tsx
+++ components/common/Tooltip.tsx
@@ -5,7 +5,7 @@ interface LoginTooltipProps {
 export function Tooltip({ label }: LoginTooltipProps) {
   return (
     <div className="absolute -top-9 left-1/2 -translate-x-1/2 opacity-90">
-      <div className="flex h-7.5 w-32.5 items-center justify-center rounded-[20px] bg-[#3D3D3D] py-1.5 pr-3.75 pl-3.75 text-xs font-medium text-white">
+      <div className="rounded-[20px] bg-[#3D3D3D] px-3.75 py-1.5 text-xs font-medium whitespace-nowrap text-white">
         {label}
       </div>
       <div className="mx-auto h-0 w-0 border-x-12 border-t-16 border-x-transparent border-t-[#3D3D3D]" />

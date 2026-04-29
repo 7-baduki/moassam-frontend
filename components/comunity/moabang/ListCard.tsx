5be3a021e452bb5b9d7ba4e1e7cdc1173fed71a8
youngsoon12 <enejwk789@gmail.com>
2026-04-29 12:10:55 +0900

    feature: 아이콘 배럴 타입 생성 (#30)

--- components/comunity/moabang/ListCard.tsx
+++ components/comunity/moabang/ListCard.tsx
@@ -18,7 +18,7 @@ export default function ListCard({ post }: ListCardProps) {
         </div>
       )}
       {/* contents Area */}
-      <div className="flex flex-col p-6 px-5">
+      <div className="flex flex-col px-5 py-6">
         <div className="flex gap-2">
           <Badge label={post.categoryName} variant="pink-light" />
           {post.tags?.map((tag) => (
@@ -28,7 +28,9 @@ export default function ListCard({ post }: ListCardProps) {
         <h3 className="typo-line-m4 mt-2.5 line-clamp-1 text-base font-semibold text-black-800">
           {post.title}
         </h3>
-        <p className="typo-line-m2 mt-4 text-sm font-semibold text-black-600">{post.authorName}</p>
+        <p className="typo-line-m2 mt-4 truncate text-sm font-semibold text-black-600">
+          {post.authorName}
+        </p>
         <div className="mt-1.5 flex items-center justify-between text-xs text-black-500">
           <div className="typo-line-p2 flex items-center gap-3">
             {post.viewCount !== undefined && (

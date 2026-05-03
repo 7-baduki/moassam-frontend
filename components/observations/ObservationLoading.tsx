79f4875641950bf52b0e3573b1f83418f1c9e520
jjinheeWorld <wlsgml450@naver.com>
2026-05-04 00:18:00 +0900

    fix: ObservationLoading 상단 패딩 수정 (#32)

--- components/observations/ObservationLoading.tsx
+++ components/observations/ObservationLoading.tsx
@@ -4,7 +4,7 @@ import Spinner from '@/components/common/spinner/Spinner';
 
 export default function ObservationLoading() {
   return (
-    <div className="flex flex-col items-center gap-5 pt-79.5">
+    <div className="flex flex-col items-center gap-5 pt-54.5">
       <Image src={observationMascot} alt="관찰일지 로딩 캐릭터" width={121} height={130} />
       <div className="flex flex-col items-center gap-0.5 text-center text-black">
         <p className="text-xl font-semibold">관찰일지를 준비하고 있어요</p>

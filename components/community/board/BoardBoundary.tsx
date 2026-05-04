a2ea0064e169e7f412fbb338000eebe2db1badba
jjinheeWorld <wlsgml450@naver.com>
2026-05-05 00:22:20 +0900

    style: BoardBoundary 로딩 UI 상단 패딩 적용 (#55)

--- components/community/board/BoardBoundary.tsx
+++ components/community/board/BoardBoundary.tsx
@@ -6,7 +6,7 @@ import BoardSection from './BoardSection';
 export default function BoardBoundary() {
   return (
     <AsyncBoundary
-      pendingFallback={<LoadingSpinner />}
+      pendingFallback={<LoadingSpinner className="pt-11.25" />}
       rejectedFallback={({ error, reset }) => (
         <ErrorFallback error={error} actionLabel="다시 시도" onAction={reset} />
       )}

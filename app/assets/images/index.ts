d2131cf583fe02ab786f5b40a274cbdf72a11b5f
jjinheeWorld <wlsgml450@naver.com>
2026-05-05 06:04:12 +0900

    chore: A, Ssam Mascot 이미지 추가 (#59)

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,3 +1,5 @@
-export { default as ErrorMascot } from './error-mascot.png';
+export { default as AMascot } from './a-mascot.png';
 export { default as DefaultAvatar } from './default-avatar.png';
+export { default as ErrorMascot } from './error-mascot.png';
 export { default as LoadingMascot } from './loading-mascot.png';
+export { default as SsamMascot } from './ssam-mascot.png';

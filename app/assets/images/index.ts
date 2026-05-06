393d8854a19d866ab2ae3217d01d0ba4a55662d8
jjinheeWorld <wlsgml450@naver.com>
2026-05-06 09:09:18 +0900

    chore: Withdraw Mascot 이미지 추가 (#59)

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -3,3 +3,4 @@ export { default as DefaultAvatar } from './default-avatar.png';
 export { default as ErrorMascot } from './error-mascot.png';
 export { default as LoadingMascot } from './loading-mascot.png';
 export { default as SsamMascot } from './ssam-mascot.png';
+export { default as WithdrawMascot } from './withdraw-mascot.png';

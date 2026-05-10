f5d7aebcfda88fcdbd91439fb3ab56f3973c128b
jjinheeWorld <wlsgml450@naver.com>
2026-05-10 17:21:24 +0900

    refactor: axios baseURL을 프록시 경로로 변경 (#65)

--- api/axios.ts
+++ api/axios.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: '/api/proxy',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
 });
 
 apiClient.interceptors.response.use(

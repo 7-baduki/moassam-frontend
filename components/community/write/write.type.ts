a34756c5d1862830a60acc312463ced951c689b0
youngsoon12 <enejwk789@gmail.com>
2026-05-13 10:14:15 +0900

    fix: 필드명, 타입 수정 및 적용 (#81)

--- components/community/write/write.type.ts
+++ components/community/write/write.type.ts
@@ -1,3 +1,6 @@
+import type { PostAge, ResourceType } from '@/components/community/moabang/moabang.type';
+import type { HeadTag } from '@/components/community/board/board.type';
+
 export type BoardType = 'moabang' | 'free';
 
 export interface WriteFormValues {
@@ -5,7 +8,7 @@ export interface WriteFormValues {
   content: string;
   files: File[];
   boardType: BoardType;
-  age?: string;
-  materialType?: string;
-  topic?: string;
+  postAge?: PostAge;
+  resourceType?: ResourceType;
+  headTag?: HeadTag;
 }

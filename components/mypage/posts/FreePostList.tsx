af1c81f9ff83f0d0f5126bc0647a7543a051398e
jjinheeWorld <wlsgml450@naver.com>
2026-05-14 18:09:49 +0900

    fix: 자유게시판 라우트 경로 수정 (#79)

--- components/mypage/posts/FreePostList.tsx
+++ components/mypage/posts/FreePostList.tsx
@@ -33,7 +33,7 @@ export default function FreePostList({ currentPage, onPageChange }: FreePostList
         {data?.data.map((post) => (
           <Link
             key={post.postId}
-            href={`/community/free/${post.postId}`}
+            href={`/community/board/${post.postId}`}
             className="flex items-center rounded-[10px] bg-white px-5 py-3 transition-colors hover:bg-black-200"
           >
             <span className="min-w-0 flex-1 truncate text-sm font-medium text-black-800">

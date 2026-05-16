4aecb945e372e95fd2847277232537a79889bc0f
jjinheeWorld <wlsgml450@naver.com>
2026-05-16 17:44:35 +0900

    style: 관찰일지 레이아웃 생성횟수 배지 반응형 적용 (#92)

--- app/observations/layout.tsx
+++ app/observations/layout.tsx
@@ -6,13 +6,13 @@ export default function ObservationsLayout({ children }: { children: React.React
   return (
     <div className="flex h-full">
       <ObservationSidebar />
-      <div className="relative flex-1 overflow-y-auto px-20 pt-25 pb-15">
-        <div className="absolute top-6 right-20">
+      <div className="relative flex-1 overflow-y-auto">
+        <div className="absolute top-4 left-1/2 -translate-x-1/2 md:top-5 md:right-8 md:left-auto md:translate-x-0 xl:top-6 xl:right-20">
           <Suspense>
             <ObservationCreditBadgeWrapper />
           </Suspense>
         </div>
-        {children}
+        <div className="px-20 pt-25 pb-15">{children}</div>
       </div>
     </div>
   );

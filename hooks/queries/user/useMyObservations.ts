e39eb56097a10cfdd3c7778739e6a792a1e3f921
jjinheeWorld <wlsgml450@naver.com>
2026-05-18 07:51:42 +0900

    feature: 마이페이지 관찰일지 내역 무한스크롤 훅 추가 (#95)

--- hooks/queries/user/useMyObservations.ts
+++ hooks/queries/user/useMyObservations.ts
@@ -1,9 +1,18 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { getMyObservations } from '@/api/user.api';
 
 export const useMyObservationsQuery = (page: number) => {
   return useSuspenseQuery({
     queryKey: ['myObservations', page],
     queryFn: () => getMyObservations(page),
+  });
+};
+
+export const useMyObservationsInfiniteQuery = () => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ['myObservations', 'infinite'],
+    queryFn: ({ pageParam = 0 }) => getMyObservations(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.hasNext ? lastPage.page + 1 : undefined),
   });
 };

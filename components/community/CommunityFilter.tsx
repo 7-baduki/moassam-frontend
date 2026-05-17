914b93994590edd4c72e77285e1a7f8def30fcf4
youngsoon12 <enejwk789@gmail.com>
2026-05-18 05:22:41 +0900

    style: 커뮤니티 필터 탭바 반응형 수정 (#96)

--- components/community/CommunityFilter.tsx
+++ components/community/CommunityFilter.tsx
@@ -24,13 +24,20 @@ export default function CommunityFilter({
   const showCategoryTabs = Boolean(categoryTabs && onCategoryChange);
 
   return (
-    <div className="mb-7.5 flex h-10 items-stretch rounded-lg bg-black-200 px-5">
-      {showAgeTabs && <Tabs options={ageTabs!} value={age} onChange={onAgeChange!} />}
+    <div className="mb-7.5 flex h-10 items-stretch overflow-x-auto rounded-lg bg-black-200 px-5 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+      {showAgeTabs && (
+        <Tabs options={ageTabs!} value={age} onChange={onAgeChange!} className="shrink-0" />
+      )}
       {showAgeTabs && showCategoryTabs && (
-        <div className="mx-7.5 h-4 w-px self-center bg-black-700" />
+        <div className="mx-7.5 h-4 w-px shrink-0 self-center bg-black-700" />
       )}
       {showCategoryTabs && (
-        <Tabs options={categoryTabs!} value={category} onChange={onCategoryChange!} />
+        <Tabs
+          options={categoryTabs!}
+          value={category}
+          onChange={onCategoryChange!}
+          className="shrink-0"
+        />
       )}
     </div>
   );

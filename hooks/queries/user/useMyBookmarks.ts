933e401727445d1bf9bfce1b49c2f544b7437780
jjinheeWorld <wlsgml450@naver.com>
2026-05-18 08:23:55 +0900

    feature: 마이페이지 북마크 무한스크롤 훅 추가 (#95)

--- hooks/queries/user/useMyBookmarks.ts
+++ hooks/queries/user/useMyBookmarks.ts
@@ -1,4 +1,4 @@
-import { useSuspenseQuery, useMutation } from '@tanstack/react-query';
+import { useSuspenseQuery, useSuspenseInfiniteQuery, useMutation } from '@tanstack/react-query';
 import { getMyBookmarks } from '@/api/bookmark.api';
 import { unbookmarkPost } from '@/api/community.api';
 
@@ -6,6 +6,15 @@ export const useMyBookmarksQuery = (page: number) => {
   return useSuspenseQuery({
     queryKey: ['myBookmarks', page],
     queryFn: () => getMyBookmarks(page),
+  });
+};
+
+export const useMyBookmarksInfiniteQuery = () => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ['myBookmarks', 'infinite'],
+    queryFn: ({ pageParam = 0 }) => getMyBookmarks(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.hasNext ? lastPage.page + 1 : undefined),
   });
 };
 

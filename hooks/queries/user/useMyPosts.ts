e3b4156d3f41c0a03159d2565e08a6d3a692be3f
jjinheeWorld <wlsgml450@naver.com>
2026-05-18 08:45:09 +0900

    feature: 마이페이지 게시글 무한스크롤 훅 추가 (#95)

--- hooks/queries/user/useMyPosts.ts
+++ hooks/queries/user/useMyPosts.ts
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { getMyMoabangPosts, getMyFreePosts } from '@/api/post.api';
 
 export const useMyMoabangPostsQuery = (page: number) => {
@@ -12,5 +12,23 @@ export const useMyFreePostsQuery = (page: number) => {
   return useSuspenseQuery({
     queryKey: ['myFreePosts', page],
     queryFn: () => getMyFreePosts(page),
+  });
+};
+
+export const useMyMoabangPostsInfiniteQuery = () => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ['myMoabangPosts', 'infinite'],
+    queryFn: ({ pageParam = 0 }) => getMyMoabangPosts(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.hasNext ? lastPage.page + 1 : undefined),
+  });
+};
+
+export const useMyFreePostsInfiniteQuery = () => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ['myFreePosts', 'infinite'],
+    queryFn: ({ pageParam = 0 }) => getMyFreePosts(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.hasNext ? lastPage.page + 1 : undefined),
   });
 };

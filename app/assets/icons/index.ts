2e3302bebeda78266d9411e8ed60ed45cc0bdc05
youngsoon12 <enejwk789@gmail.com>
2026-05-18 04:32:06 +0900

    chore: 아이콘 추가 (#96)

--- app/assets/icons/index.ts
+++ app/assets/icons/index.ts
@@ -29,3 +29,5 @@ export { default as DetailHeartIcon } from './DetailHeartIcon.svg';
 export { default as DownloadIcon } from './DownloadIcon.svg';
 export { default as TopIcon } from './TopIcon.svg';
 export { default as HamburgerIcon } from './HamburgerIcon.svg';
+export { default as BackArrowIcon } from './BackArrowIcon.svg';
+export { default as InfoIcon } from './infoIcon.svg';

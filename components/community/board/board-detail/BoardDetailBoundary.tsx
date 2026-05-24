97bcf4d9c5834de2c61b3b1e1ca0c3e520931a64
youngsoon12 <enejwk789@gmail.com>
2026-05-25 04:01:33 +0900

    fix: 게시글 상세 인증 체크를 서버 쿠키로 이동해 hydration mismatch 해결 (#138)

--- components/community/board/board-detail/BoardDetailBoundary.tsx
+++ components/community/board/board-detail/BoardDetailBoundary.tsx
@@ -5,8 +5,13 @@ import BoardDetailSection from './BoardDetailSection';
 interface BoardDetailBoundaryProps {
   postId: number;
   title: string;
+  initialAuth: boolean;
 }
 
-export default function BoardDetailBoundary({ postId, title }: BoardDetailBoundaryProps) {
-  return <BoardDetailSection postId={postId} title={title} />;
+export default function BoardDetailBoundary({
+  postId,
+  title,
+  initialAuth,
+}: BoardDetailBoundaryProps) {
+  return <BoardDetailSection postId={postId} title={title} initialAuth={initialAuth} />;
 }

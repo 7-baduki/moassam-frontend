b9a83e32466074916c616c808da6bdc4255929e3
jjinheeWorld <jjinheeworld@gmail.com>
2026-09-02 03:59:37 +0900

    fix: Vercel Serverless Functions 개수 감소를 위해 force-static 적용 (#166)

--- app/(legal)/terms/page.tsx
+++ app/(legal)/terms/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import LegalHeader from '@/components/legal/LegalHeader';
 
+export const dynamic = 'force-static';
+
 export const metadata: Metadata = {
   title: '이용약관',
 };
